520aac8e9f013432e84eaa727bd451d3759118e2
Torey Hickman <torey@toreyhickman.com>
2014-09-26 11:35:53 -0400

    Initial class methods written
    Garoupa.make_groups will make groups from a list (an Array) and pass the groups on to Garoupa.new to instantiate a new Garoupa instance.  The method allows for some optional parameters:  target groups size, max difference in group sizes in case the groups aren't even, and history of past groups so that groups can be mad with a preference for placing list items with new groupmates.
    
    Garoupa.past_groupmates takes a list and previous group history and returns a Hash object.  Keys are the list items.  Values are collections of list items that have been groupmates of the key.

--- lib/garoupa.rb
+++ lib/garoupa.rb
@@ -1,5 +1,87 @@
 require "garoupa/version"
 
 class Garoupa
-  # Your code goes here...
+
+  def self.make_groups(list, options = {})
+    list_items_past_groupmates = past_groupmates(list, options[:past_groups])
+    group_structure            = make_empty_group_structure(list.size, options[:target_size])
+    corrected_group_structure  = correct_for_group_size_difference(group_structure, options[:max_difference])
+    groups                     = fill_group_structure(group_structure, list.shuffle, list_items_past_groupmates)
+
+    self.new(groups, list, list_items_past_groupmates)
+  end
+
+  def self.past_groupmates(list, past_groups)
+    list.each_with_object( Hash.new ) do |list_item, previous_pairs|
+      previous_pairs[list_item] = past_groupmates_for(list_item, past_groups)
+    end
+  end
+
+
+  attr_reader :groups
+
+  def initialize(groups, list, past_groupmates)
+    @groups          = groups
+    @list            = list
+    @past_groupmates = past_groupmates
+  end
+
+  private
+  def self.divide_list(list, group_size = nil)
+    list.each_slice(group_size || 4).to_a
+  end
+
+  def self.correct_for_group_size_difference(groups, max_difference = nil)
+    return groups unless max_difference
+
+    if difference_in_group_sizes(groups) > max_difference
+      groups = disperse_last_group(groups)
+    end
+    groups
+  end
+
+  def self.difference_in_group_sizes(groups)
+    sizes = groups.map(&:size)
+    sizes.max - sizes.min
+  end
+
+  def self.disperse_last_group(groups)
+    last_group = groups.pop
+
+    last_group.each_with_index do |element, index|
+      groups[index] << element
+    end
+
+    groups
+  end
+
+  def self.make_empty_group_structure(list_size, target_size = nil)
+    empty_list = Array.new(list_size) { nil }
+    empty_groups = divide_list(empty_list, target_size)
+  end
+
+  def self.past_groupmates_for(element, past_groups = nil)
+    return [] unless past_groups
+
+    past_groups = past_groups.select { |group| group.include? element }
+    past_groupmates = past_groups.flatten.reject { |el| el == element }
+  end
+
+  def self.fill_group_structure(group_structure, list, previous_pairs = {})
+    list.each do |list_item|
+      place_in_best_group(list_item, group_structure, previous_pairs)
+    end
+
+    group_structure
+  end
+
+  def self.place_in_best_group(list_item, group_structure, previous_pairs)
+    available_groups = group_structure.select { |group| group.include? nil }
+    group_with_least_repeats = available_groups.min_by { |group| group & previous_pairs[list_item] }
+
+    index_of_nil = group_with_least_repeats.index nil
+    group_with_least_repeats[index_of_nil] = list_item
+
+    return nil
+  end
 end
--- spec/garoupa_spec.rb
+++ spec/garoupa_spec.rb
@@ -1,1 +1,110 @@
 require 'spec_helper'
+
+describe Garoupa do
+
+  let(:list) { [:a, :b, :c, :d, :e, :f, :g, :h, :i, :j, :k, :l] }
+  let(:no_options_groups) { Garoupa.make_groups(list) }
+
+  describe ".make_groups" do
+    it "returns a Groupa object" do
+      expect(no_options_groups).to be_instance_of Garoupa
+    end
+
+    describe "group_sizes" do
+      context "no target size given" do
+        it "makes groups of four" do
+          groups_sizes = no_options_groups.groups.map(&:size)
+          expect(groups_sizes.all? { |s| s == 4 }).to be true
+        end
+      end
+
+      context "target size given" do
+        context "list size evenly divisible by target size" do
+          it "makes groups of a specified size" do
+            options = { target_size: 3 }
+            groups = Garoupa.make_groups(list, options)
+            groups_sizes = groups.groups.map(&:size)
+
+            expect(groups_sizes.all? { |s| s == 3 }).to be true
+          end
+        end
+
+        context "list size not evenly divisible by target size" do
+          context "no max size difference specified" do
+            it "has one group with a different size" do
+              options = { target_size: 5 }
+              groups = Garoupa.make_groups(list, options)
+              groups_sizes = groups.groups.map(&:size)
+
+              expect(groups_sizes).to match_array [5, 5, 2]
+            end
+          end
+
+          context "max size difference specified" do
+            context "max size difference not exceeded" do
+              it "leaves the smaller group" do
+                options = { target_size: 5, max_difference: 3 }
+                groups = Garoupa.make_groups(list, options)
+                groups_sizes = groups.groups.map(&:size)
+
+                expect(groups_sizes).to match_array [5, 5, 2]
+              end
+            end
+            context "max size difference exceeded" do
+              it "disperses the smaller group" do
+                options = { target_size: 5, max_difference: 2 }
+                groups = Garoupa.make_groups(list, options)
+                groups_sizes = groups.groups.map(&:size)
+
+                expect(groups_sizes).to match_array [6, 6]
+              end
+            end
+          end
+        end
+      end
+    end
+
+    describe "group make up" do
+      context "no past groups provided" do
+        it "divides shuffled list into groups" do
+          allow(list).to receive(:shuffle) do
+            [:a, :l, :c, :k, :e, :j, :g, :h, :i, :f, :d, :b]
+          end
+          expected_groups = [[:a, :l, :c, :k], [:e, :j, :g, :h], [:i, :f, :d, :b]]
+
+          expect(no_options_groups.groups).to match_array expected_groups
+        end
+      end
+
+      context "past groups provided" do
+        let(:past_groups) { [[:a, :b], [:a, :c], [:a, :d], [:a, :e], [:a, :f], [:a, :g], [:a, :h], [:a, :i]] }
+
+        it "attempts to make groups with minimal repeats" do
+          allow(list).to receive(:shuffle) do
+            [:a, :b, :c, :d, :e, :f, :g, :h, :i, :j, :k, :l]
+          end
+
+          options = { past_groups: past_groups }
+          groups = Garoupa.make_groups(list, options).groups
+
+          a_group = groups.find { |group| group.include? :a }
+          expect(a_group).to match_array [:a, :j, :k, :l]
+        end
+      end
+    end
+  end
+
+  describe ".past_groupmates" do
+    let(:list) { [:a, :b, :c, :d] }
+    let(:past_groups) { [[:a, :b], [:a, :c]] }
+
+    it "maps list items to their previous groupmates" do
+      past_group_map = Garoupa.past_groupmates(list, past_groups)
+
+      expect(past_group_map[:a]).to match_array [:b, :c]
+      expect(past_group_map[:b]).to match_array [:a]
+      expect(past_group_map[:c]).to match_array [:a]
+      expect(past_group_map[:d]).to be_empty
+    end
+  end
+end
