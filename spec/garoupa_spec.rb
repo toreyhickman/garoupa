ca4488b9c4dda42b170a5617199db2288cee3e6c
Torey Hickman <torey@toreyhickman.com>
2014-09-29 15:38:03 -0400

    Refactor tests

--- spec/garoupa_spec.rb
+++ spec/garoupa_spec.rb
@@ -2,19 +2,30 @@ require 'spec_helper'
 
 describe Garoupa do
 
-  let(:list) { [:a, :b, :c, :d, :e, :f, :g, :h, :i, :j, :k, :l] }
-  let(:no_options_groups) { Garoupa.make_groups(list) }
+  let(:long_list) { [:a, :b, :c, :d, :e, :f, :g, :h, :i, :j, :k, :l] }
+  let(:garoupa_no_options) { Garoupa.make_groups(long_list) }
+
+  let(:garoupa) { Garoupa.new(groups, list, past_groupmates) }
+  let(:groups) { [[:a, :b], [:c, :d]] }
+  let(:list) { [:a, :b, :c, :d] }
+  let(:past_groupmates) do
+    { :a => [:b, :c, :d],
+      :b => [:a],
+      :c => [],
+      :d => [] }
+  end
+
 
   describe ".make_groups" do
     it "returns a Groupa object" do
-      expect(no_options_groups).to be_instance_of Garoupa
+      expect(garoupa_no_options).to be_instance_of Garoupa
     end
 
     describe "group_sizes" do
       context "no target size given" do
-        it "makes groups of four" do
-          groups_sizes = no_options_groups.groups.map(&:size)
-          expect(groups_sizes.all? { |s| s == 4 }).to be true
+        it "attemtps to make groups of the default group size" do
+
+          expect(garoupa_no_options.groups.first.size).to eq Garoupa::DEFAULT_GROUP_SIZE
         end
       end
 
@@ -22,7 +33,7 @@ describe Garoupa do
         context "list size evenly divisible by target size" do
           it "makes groups of a specified size" do
             options = { target_size: 3 }
-            groups = Garoupa.make_groups(list, options)
+            groups = Garoupa.make_groups(long_list, options)
             groups_sizes = groups.groups.map(&:size)
 
             expect(groups_sizes.all? { |s| s == 3 }).to be true
@@ -33,7 +44,7 @@ describe Garoupa do
           context "no max size difference specified" do
             it "has one group with a different size" do
               options = { target_size: 5 }
-              groups = Garoupa.make_groups(list, options)
+              groups = Garoupa.make_groups(long_list, options)
               groups_sizes = groups.groups.map(&:size)
 
               expect(groups_sizes).to match_array [5, 5, 2]
@@ -44,7 +55,7 @@ describe Garoupa do
             context "max size difference not exceeded" do
               it "leaves the smaller group" do
                 options = { target_size: 5, max_difference: 3 }
-                groups = Garoupa.make_groups(list, options)
+                groups = Garoupa.make_groups(long_list, options)
                 groups_sizes = groups.groups.map(&:size)
 
                 expect(groups_sizes).to match_array [5, 5, 2]
@@ -53,7 +64,7 @@ describe Garoupa do
             context "max size difference exceeded" do
               it "disperses the smaller group" do
                 options = { target_size: 5, max_difference: 2 }
-                groups = Garoupa.make_groups(list, options)
+                groups = Garoupa.make_groups(long_list, options)
                 groups_sizes = groups.groups.map(&:size)
 
                 expect(groups_sizes).to match_array [6, 6]
@@ -67,12 +78,12 @@ describe Garoupa do
     describe "group make up" do
       context "no past groups provided" do
         it "divides shuffled list into groups" do
-          allow(list).to receive(:shuffle) do
+          allow(long_list).to receive(:shuffle) do
             [:a, :l, :c, :k, :e, :j, :g, :h, :i, :f, :d, :b]
           end
           expected_groups = [[:a, :l, :c, :k], [:e, :j, :g, :h], [:i, :f, :d, :b]]
 
-          expect(no_options_groups.groups).to match_array expected_groups
+          expect(garoupa_no_options.groups).to match_array expected_groups
         end
       end
 
@@ -80,12 +91,12 @@ describe Garoupa do
         let(:past_groups) { [[:a, :b], [:a, :c], [:a, :d], [:a, :e], [:a, :f], [:a, :g], [:a, :h], [:a, :i]] }
 
         it "attempts to make groups with minimal repeats" do
-          allow(list).to receive(:shuffle) do
+          allow(long_list).to receive(:shuffle) do
             [:a, :b, :c, :d, :e, :f, :g, :h, :i, :j, :k, :l]
           end
 
           options = { past_groups: past_groups }
-          groups = Garoupa.make_groups(list, options).groups
+          groups = Garoupa.make_groups(long_list, options).groups
 
           a_group = groups.find { |group| group.include? :a }
           expect(a_group).to match_array [:a, :j, :k, :l]
@@ -141,7 +152,7 @@ describe Garoupa do
 
   describe "#to_json" do
     it "returns a JSON formatted string" do
-      expect(!!JSON.parse(garoupa.to_json)).to be true
+      expect { JSON.parse(garoupa.to_json) }.to_not raise_error
     end
 
     it "includes groups, list, past groupmates, and repeat pairs" do
